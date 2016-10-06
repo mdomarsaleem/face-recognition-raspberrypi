11ce892a588122fbbaf15962b410b5b7259eea7d
mdomarsaleem <omarsaleem003@gmail.com>
2016-10-06 11:42:06 +0530

    Raspi

--- file_transfer.sh
+++ file_transfer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sshpass -ppassword rsync -avP /home/pi/Desktop/Image_db/* mckc@192.168.100.94:/home/mckc/Raspberry
+sshpass -pPassword14 rsync -avP /home/pi/Desktop/Image_db/* mckc@192.168.100.94:/home/mckc/Raspberry
 
 ping -c 1 192.168.100.94 &> /dev/null && rm -r /home/pi/Desktop/Image_db/* || echo fail
---
+++ rasp_on.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+export DISPLAY=:0
+export XAUTHORITY=/home/pi/.Xauthority
+python /home/pi/Desktop/files/database_cv.py
