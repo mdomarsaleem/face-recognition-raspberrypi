24e2bc345b6989c1afa5e8b66c91ed6d270e16be
Omar Saleem Mohammed <Omar Saleem Mohammed@MCKINSEY.COM>
2017-04-19 18:57:56 +0530

    cleaning

--- file_transfer.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sshpass -p(password) rsync -avP /home/pi/Desktop/Image_db/* (username)@(server ip address):/home/mckc/Raspberry
-
-ping -c 1 (server ip) &> /dev/null && rm -r /home/pi/Desktop/Image_db/* || echo fail
--- rasp_on.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export DISPLAY=:0
-export XAUTHORITY=/home/pi/.Xauthority
-python /home/pi/Desktop/files/database_cv.py
